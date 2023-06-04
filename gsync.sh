2e9964e4a247f0ff9925c99cac6127892cf7dce4
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-06-05 04:43:09 +0530

    feat: initial working version

--- gsync.sh
+++ gsync.sh
@@ -1,13 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-for repo in $REPO_LIST; do
-    if $repo starts with /; then
-        cd "$repo" || exit
-        git pull --rebase --autostash
-        git push
-    else
-        cd "$HOME/$repo" || exit
-        git pull --rebase --autostash
-        git push
-    fi
-done
+echo "Syncing all repositories..."
+config_file="$HOME/.config/gsync"
+
+if [ ! -f "$config_file" ]; then
+    echo "Config file $config_file not found."
+    exit 1
+fi
+
+while IFS= read -r repo_path; do
+    (
+        cd "$repo_path" || exit 1
+        echo "Syncing $repo_path..."
+        (git pull --rebase --autostash && git push) >/dev/null 2>&1 &
+    ) &
+done < "$config_file"
+
+echo "All repositories have been synced."
+
